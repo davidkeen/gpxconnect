c4d4538d2a7b29215e02f60f20917d6e83d3ceda
David Keen <david.keen@amee.com>
2012-02-21 14:51:27 +0000

    Shortcode.

--- wp_communicator.php
+++ wp_communicator.php
@@ -13,16 +13,20 @@
 
 function gpx_shortcode_handler($atts, $content = null, $code = "" ) {
 
-    // Extract the shortcode arguments.
-    extract(shortcode_atts(array(
-            'name' => uniqid()
-        ), $atts));
+    // Extract the shortcode arguments into local variables (setting defaults as required)
+    extract(shortcode_atts(array('name' => uniqid()), $atts));
 
+    // Create a div to show the import button.
+    $ret = '<div id="garminDisplay">&#160;</div>';
 
+    // Write out a javascript variable for the filename
+    $ret .= "<script type='text/javascript'>var gpxFilename = $name </script>";
 
+    // Insert the contents of custom field into a hidden text area
     $gpxData = get_post_meta($post->ID, 'gpx', true);
+    $ret .= "<textarea id='dataString' style='display:none'>$gpxData</textarea>";
 
-
+    return $ret;
 }
 
 
